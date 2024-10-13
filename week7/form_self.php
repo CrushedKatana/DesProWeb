861f69598ec01eac0b9ce996adbd6ef27e9d247f
CrushedKatana <charelsadewo123@gmail.com>
2024-10-13 19:18:55 +0700

    commit

---
+++ week7/form_self.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form Input PHP</title>
+</head>
+
+<body>
+    <h2>Form Input PHP</h2>
+    <?php
+    // insialisasi varriabel
+    $namaErr = "";
+    $nama = "";
+
+    //cek apakah form sudah disubmit
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["nama"])) {
+            $namaErr = "Nama harus diisi!";
+        } else {
+            $nama = $_POST["nama"];
+            echo "Data berhasil disimpan!";
+        }
+    }
+    ?>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="nama">Nama: </label>
+        <input type="text" name="nama" id="nama" value="<?php echo $nama; ?>">
+        <span class="error">
+            <?php echo $namaErr; ?>
+        </span><br><br>
+
+        <input type="submit" name="submit" id="submit">
+    </form>
+</body>
+
+</html>
--- week7/proses_form.php
+++ week7/proses_form.php
@@ -1,9 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"]== "POST"){
-    $nama = $_POST["nama"];
-    $email = $_POST["email"];
-
-    echo "Nama: " .$nama . "<br>";
-    echo "Email: " . $email;
-}
+$input = $_POST['input'];
+$input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8'); // This escapes HTML characters
+echo "Processed Input: " . $input;
 ?>
