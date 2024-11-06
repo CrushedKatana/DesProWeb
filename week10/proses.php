a5ace33475d609c87bb506db53326f5b87d16a01
CrushedKatana <charelsadewo123@gmail.com>
2024-11-06 21:21:30 +0700

    Update proses.php

--- week10/proses.php
+++ week10/proses.php
@@ -16,6 +16,42 @@
             echo "Gagal menambahkan data: " . mysqli_error($koneksi);
         }
     }
+
+    elseif ($aksi == 'ubah') {
+        if (isset($_POST['id'])) {
+            $id = $_POST['id'];
+
+            $query = "UPDATE anggota SET nama = '$nama', jenis_kelamin = '$jenis_kelamin', alamat = '$alamat', no_telp = '$no_telp' WHERE id = '$id'";
+
+            if (mysqli_query($koneksi, $query)) {
+                header("location: index.php");
+                exit();
+            } else {
+                echo "Gagal mengupdate data: " . mysqli_error($koneksi);
+            }
+        } else {
+            echo "ID tidak valid.";
+        }
+    }
+
+    elseif ($aksi == 'hapus') {
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+    
+            $query = "DELETE FROM anggota WHERE id = '$id'";
+    
+            if (mysqli_query($koneksi, $query)) {
+                header("location: index.php");
+                exit();
+            } else {
+                echo "Gagal menghapus data: " . mysqli_error($koneksi);
+            }
+        } else {
+            echo "ID tidak valid.";
+        }
+    } else {
+        header("Location: index.php");
+    }
     
     mysqli_close($koneksi);
 ?>
